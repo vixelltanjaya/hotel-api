0cecf08dcec6634352e18b007d2bf4b21ce69b1f
Laksamana Vixell T H <laksamanavixell@gmail.com>
2022-12-15 21:24:40 +0700

    manipulate db

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from db import mysql
 import json
 from auth import auth, jsonFormatArray, jsonFormat, tokenKey, dateFormat, mail
 from datetime import datetime, timedelta
+import googlemaps
+from key import API_KEY
 
 
 app = Flask(__name__)
@@ -14,11 +16,11 @@ app.config["SECRET_KEY"]= "secret"
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'hotel_api'
+# app.config['MYSQL_DB'] = 'hotel_api'
 
 #keperluan testing
-# app.config['MYSQL_DB'] = 'TUBES'
-# app.config['MYSQL_UNIX_SOCKET'] = '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
+app.config['MYSQL_DB'] = 'TUBES'
+app.config['MYSQL_UNIX_SOCKET'] = '/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock'
 
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_SERVER"] = "imap.gmail.com"
@@ -32,6 +34,7 @@ mail.init_app(app)
 
 @app.route('/', methods=['GET'])
 def hotel():
+    getLatLong()
     return "BUTUH INFORMASI?"
 
 @app.route('/hotel', methods=['GET', 'POST'])
@@ -47,32 +50,36 @@ def hotel1():
         if (Nama_Hotel and Bintang):
             query = "SELECT * FROM hotel WHERE Nama_Hotel LIKE '%{}%' AND Bintang LIKE '%{}%' ".format(Nama_Hotel, Bintang)
             cursor.execute(query)
-            data = cursor.fetchall()
+            data = jsonFormatArray(cursor)
+
             return jsonify(data)
         
         elif(Alamat_Hotel):
             query = "SELECT * FROM hotel WHERE Alamat_Hotel LIKE '%{}%' ".format(Alamat_Hotel)
             cursor.execute(query)
-            data = cursor.fetchall()
+            data = jsonFormatArray(cursor)
+            
             return jsonify(data)
 
         elif(Bintang):
             query = "SELECT * FROM hotel WHERE Bintang LIKE '%{}%' ".format(Bintang)
             cursor.execute(query)
-            data = cursor.fetchall()
+            data = jsonFormatArray(cursor)
+
             return jsonify(data)
 
         elif(Nama_Hotel):
             query = "SELECT * FROM hotel WHERE Nama_Hotel LIKE '%{}%' ".format(Nama_Hotel)
             cursor.execute(query)
-            data = cursor.fetchall()
+            data = jsonFormatArray(cursor)
+
             return jsonify(data)
 
         else:
             cursor = mysql.connection.cursor()
             query = "SELECT * FROM hotel"
             cursor.execute(query)
-            data = cursor.fetchall()
+            data = jsonFormatArray(cursor)
 
             return jsonify(data)
 
@@ -114,6 +121,61 @@ def hotel2(id):
         mysql.connection.commit()
 
         return jsonify({"messages": "Data Berhasil Dihapus"}), 202
+
+@app.route('/dev/getlatlong', methods=['GET'])
+def manipulateDb():
+    cursor = mysql.connection.cursor()
+    
+    cursor.execute('SELECT * FROM hotel')
+    hotelData = jsonFormatArray(cursor)
+
+    for hotel in hotelData:
+        coor = getLatLong(hotel['Alamat_Hotel'])
+        if (coor != {}):
+            cursor.execute('UPDATE hotel SET latitude=%s, longitude=%s WHERE id=%s', (coor['latitude'], coor['longitude'], hotel['id']))
+            mysql.connection.commit()
+        else:
+            print('lat long NULL', hotel["id"], hotel["Nama_Hotel"])
+
+@app.route('/dev/createcol', methods=['GET'])
+def createCol():
+    cursor = mysql.connection.cursor()
+
+    cursor.execute('ALTER TABLE hotel ADD latitude VARCHAR(64), ADD longitude VARCHAR(64)')
+    mysql.connection.commit()
+
+
+def getLatLong(street):
+    gmaps_key = googlemaps.Client(key=API_KEY)
+    g = gmaps_key.geocode(street)
+    lat = g[0]["geometry"]["location"]["lat"]
+    long = g[0]["geometry"]["location"]["lng"]
+    if (lat and long):
+        print('Latitude: ', lat, ', Longitude: ', long, str(lat+long))
+        return ({"latitude": lat, "longitude": long})
+    else: 
+        return {}
+
+def jsonFormatArray(cursor):
+  headers = [x[0] for x in cursor.description]
+  data = cursor.fetchall()
+
+  res = []
+
+  for item in data:
+      res.append(dict(zip(headers, item)))
+
+  return res
+
+def jsonFormat(cursor):
+  headers = [x[0] for x in cursor.description]
+  data = cursor.fetchall()
+  res = {}
+
+  for item in data:
+    res = (dict(zip(headers, item)))
+
+  return res
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
